0f7ad3cb02aeff20506b436a51d9b74ac6af3cf9
pogerooer <r.k.pasha.aman@gmail.com>
2024-10-25 16:11:50 +1100

    Added context block of text under algorithm buttons

--- gui.py
+++ gui.py
@@ -83,10 +83,43 @@ class GUI:
                         validation={'Input too long': lambda value: len(value) < 20})
                 result = ui.label()       
                 ui.button("Predict", on_click= lambda:Predict(int(value.value)))         
-                ui.button("Linear Regression", on_click=linearRegression)
-                ui.button("Decision Tree", on_click=decisionTree)
-                ui.button("Random Forest", on_click=randomForest)
-                ui.button("SVM Regressor", on_click=svmRegressor)
+                with ui.card():
+                    ui.button("Linear Regression", on_click=linearRegression)
+
+                    linear_regression_output = ui.markdown("""
+                        The simplest, and most fundamental predictive model. 
+                        It assumes a linear relationship between the dependent and independent variables. 
+                        It is incredibly easy to interpret and utilises extrapolation to make predictive analysis. 
+                        However, it sometimes fails to capture more complex data patterns.
+                    """)
+                linear_regression_output.style = "overflow-y: auto; height: 200px;"
+                with ui.card():
+                    ui.button("Decision Tree", on_click=decisionTree)
+                    decision_tree_output = ui.markdown("""
+                        These models use a tree-like structure to make predictions, hence the name. 
+                        Each node represents a decision rule, while each leaf node corresponds to a final prediction. 
+                        Although decision trees are easy to understand and implement, they are highly prone to overfitting. 
+                        This means they may perform well on training data but often struggle to generalize effectively to new, unseen data. 
+                        However, decision trees are incredibly important, in the sense that they are foundational to the implementation of other predictive models.
+                    """)
+                    decision_tree_output.style = "overflow-y: auto; height: 200px;"
+                with ui.card():
+                    ui.button("Random Forest", on_click=randomForest)
+                    random_forest_output = ui.markdown("""
+                        To solve the issues of overfitting, this model combines multiple simple decision trees into a more complicated structure. 
+                        This increases redundancy and leads to a more accurate predictive model. 
+                        However, this algorithm can prove to be slow and ineffective, especially if a large number of trees are used.
+                    """)
+                    random_forest_output.style = "overflow-y: auto; height: 200px;"
+                with ui.card():
+                    ui.button("SVM Regressor", on_click=svmRegressor)
+                    svm_regressor_output = ui.markdown("""
+                        Support Vector Machines works via the implementation of an optimal hyperplane that separates data points based on their classification. 
+                        This method utilises kernel functions to handle high-dimensional data and works well with linear and non-linear data. 
+                        It is especially useful in datasets with several classifications. 
+                        However, this model requires extensive training, and the final model is often difficult to interpret.
+                    """)
+                    svm_regressor_output.style = "overflow-y: auto; height: 200px;"
             with ui.column():
                 ui.label("Table of Data")
                 with ui.card():
